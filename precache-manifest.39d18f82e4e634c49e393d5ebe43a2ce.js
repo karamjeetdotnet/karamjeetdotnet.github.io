a70dc9892c4e4e490ab8c15b13dc091b1d834a83
karamjeetdotnet <karamjeet2004@hotmail.com>
2019-06-15 17:22:41 +0530

    site deploy

---
+++ precache-manifest.39d18f82e4e634c49e393d5ebe43a2ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7cf5ed0ce7a0f994152c57cc32d81de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "2b971975b8cfb54d3dc8",
+    "url": "/static/js/main.7a306ae6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
